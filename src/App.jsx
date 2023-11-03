9b8131840349b88d6cc8ff771d6cce9df390d348
Brett Favro <59139234+bmf87@users.noreply.github.com>
2023-11-03 00:49:21 +0000

    Add dependencies and update siteProps and image

--- src/App.jsx
+++ src/App.jsx
@@ -23,16 +23,17 @@ import "./styles.css";
  *
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
+
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Brett Favro",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "bfavro73@gmail.com",
+  gitHub: "Kbmf87",
+  instagram: "BrettFavro",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "favB_12",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
